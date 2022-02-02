17fe52cb86c8b784655af913ae57c93476a555e5
Milanparajuli <milanbhai2058@gmail.com>
2022-02-02 13:54:51 +0545

    add category list

--- post/views.py
+++ post/views.py
@@ -1,12 +1,21 @@
-from django.shortcuts import render,redirect
-from .models import Category
+    from django.shortcuts import render,redirect
+    from .models import Category
+    from django.contrib import messages
+    # Create your views here.
+    def category(request):
+        if request.method=='GET':
+            cat = Category.objects.all() #select *
+            contex={
+                'categories':cat,
+            }
+            return render(request,'category.html',contex)
+        else:
+            t=request.POST['category_title']
+            try:
+                cat = Category(title = t)
+                cat.save()
+                messages.add_message(request,messages.SUCCESS,"saved")
+            except Exception as e:
+                messages.add_message(request, messages.ERROR, e)
+            return redirect('category')
 
-# Create your views here.
-def category(request):
-    if request.method=='GET':
-        return render(request,'category.html')
-    else:
-        t=request.POST['categorytitle']
-        cat = category(title = t)
-        cat.save()
-        return redirect('category')
