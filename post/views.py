f66801ad8f1e52f69f357ab412cb2ae46c9fadb1
Milanparajuli <milanbhai2058@gmail.com>
2022-02-02 12:50:15 +0545

    2feb

--- post/views.py
+++ post/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Category
 
 # Create your views here.
 def category(request):
-    return render(request,'category.html')
+    if request.method=='GET':
+        return render(request,'category.html')
+    else:
+        t=request.POST['categorytitle']
+        cat = category(title = t)
+        cat.save()
+        return redirect('category')
